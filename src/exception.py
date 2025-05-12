9f8a899fa5f4bafa0db05d7b13b71b19f8b861db
MrSMittal <mittalshivam40@gmail.com>
2025-05-12 02:43:54 +0000

    v1.1

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exec_tb=error_detail.exc_info()
@@ -14,3 +15,10 @@ class custom_exception(Exception):
 
     def __Str__(self):
         return self.error_message
+    
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide By Zero")
+        raise custom_exception(e,sys)
